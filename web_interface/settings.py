3d9aa7a006ec61c7c335f5952802c3c12400a550
Joel Cambon <joel2.718@gmail.com>
2011-11-24 00:36:19 -0500

    fixed issue with filebrowser

--- web_interface/settings.py
+++ web_interface/settings.py
@@ -1,5 +1,6 @@
 # Django settings for mysite project.
-
+import os
+import sys
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -45,34 +46,44 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT ='' 
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = ''
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/home/joel/tralf/django-tralf-interface/web_interface/static'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+) 
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'y=$-)y*4efq2j=x(h-r208444*7i+_=s+*l#$*b6c_e&_@bype'
 
-STATICFILES_DIRS = (
-    "/home/joel/tralf_root",
-)
-
-STATIC_ROOT = (
-    "/home/joel/tralf/django-tralf-interface/web_interface/tralf_static"
-)
-
-STATIC_URL = (
-    "/static/"
-)
-
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -89,6 +100,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'web_interface.urls'
 
+
 TEMPLATE_DIRS = (
     "/home/joel/tralf/django-tralf-interface/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -99,16 +111,66 @@ TEMPLATE_DIRS = (
 INSTALLED_APPS = (
     'grappelli',
     'filebrowser',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.admin',
-    'player',
     'django.contrib.staticfiles',
+    'player',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+
+FILEBROWSER_MEDIA_ROOT = MEDIA_ROOT
+FILEBROWSER_MEDIA_URL = MEDIA_URL
+FILEBROWSER_DIRECTORY = 'media/uploads/'
+FILEBROWSER_URL_FILEBROWSER_MEDIA = STATIC_URL + "filebrowser/"
+FILEBROWSER_PATH_FILEBROWSER_MEDIA = os.path.join(STATIC_ROOT, 'filebrowser/')
+
+#FILEBROWSER_URL_TINYMCE = ADMIN_MEDIA_PREFIX + "tinymce/jscripts/tiny_mce/"
+#FILEBROWSER_PATH_TINYMCE =ADMIN_MEDIA_PREFIX + "tinymce/jscripts/tiny_mce/"
+
+
+FILEBROWSER_EXTENSIONS={
+    'Image': ['.jpg','.jpeg','.gif','.png','.tif','.tiff'],
+    'Document': ['.pdf','.doc','.rtf','.txt','.xls','.csv'],
+    'Video': ['.mov','.wmv','.mpeg','.mpg','.avi','.rm'],
+    'Audio': ['.mp3','.mp4','.wav','.aiff','.midi','.m4p']
+}
+
+FILEBROWSER_SELECT_FORMATS =  {
+    'file': ['Folder','Image','Document','Video','Audio'],
+    'image': ['Image'],
+    'document': ['Document'],
+    'media': ['Video','Audio'],
+}
+
+
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
