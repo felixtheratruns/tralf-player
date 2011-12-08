5a7fdd83455960cf3ece938eaaaa66917d5a5db7
Joel Cambon <joel2.718@gmail.com>
2011-12-08 13:05:10 -0500

    added some module tests

--- web_interface/player/tests/test_models.py
+++ web_interface/player/tests/test_models.py
@@ -1,6 +1,21 @@
-import unittest
+from django.test import TestCase
+#stuff added by programmer is below:
+from django.utils import unittest
+from player.models import Player
+from datetime import datetime
 
+
+question="Do you like "+ "test_file" +" ?"
+pub_date=datetime.now()
+file_name="test_file"
+frame_num_start=0
+frame_num_stop=0
+username = 'test'
+ 
 class TestPlayer(unittest.TestCase):
+    def setUp(self):
+        self.player1 = Player(username=username,question=question,  pub_date=pub_date,  file_name=file_name, frame_num_start=frame_num_start, frame_num_stop=frame_num_stop)
+ 
     def test___unicode__(self):
         # player = Player()
         # self.assertEqual(expected, player.__unicode__())
@@ -10,6 +25,18 @@ class TestPlayer(unittest.TestCase):
         # player = Player()
         # self.assertEqual(expected, player.was_published_today())
         assert False # TODO: implement your test here
+
+    def testAttrib(self):
+        self.assertEqual(self.player1.username, username)
+        self.assertEqual(self.player1.question, question)
+        self.assertEqual(self.player1.pub_date, pub_date)
+        self.assertEqual(self.player1.file_name, file_name)
+        self.assertEqual(self.player1.frame_num_start, frame_num_start)
+        self.assertEqual(self.player1.frame_num_stop, frame_num_stop)
+
+
+
+
 
 class TestChoice(unittest.TestCase):
     def test___unicode__(self):
